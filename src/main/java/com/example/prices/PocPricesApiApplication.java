83dcc852a0d136173924548e2d758900e24cacfc
Antonio D. G. Medina <antgarmed@outlook.com>
2025-09-03 18:41:11 +0200

    feat: add UseCase annotation and update component scanning in application

--- src/main/java/com/example/prices/PocPricesApiApplication.java
+++ src/main/java/com/example/prices/PocPricesApiApplication.java
@@ -2,8 +2,15 @@ package com.example.prices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import com.example.prices.shared.application.UseCase;
 
 @SpringBootApplication
+@ComponentScan(includeFilters = {
+		@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = UseCase.class)
+})
 public class PocPricesApiApplication {
 
 	public static void main(String[] args) {
