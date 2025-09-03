2e43d7fbcd446b1d4209d101964f333701c05257
Antonio D. G. Medina <antgarmed@outlook.com>
2025-09-03 17:21:34 +0200

    feat: initialize project

---
+++ src/main/java/com/example/prices/PocPricesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPricesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPricesApiApplication.class, args);
+	}
+
+}
